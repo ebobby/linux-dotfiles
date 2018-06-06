12f964946f48951db03f65effe4fdc099bfed904
Francisco Soto <ebobby@ebobby.org>
2018-06-06 15:57:17 -0700

    Changed directories.

--- local.sh
+++ local.sh
@@ -1,5 +1,5 @@
 alias home-wifi="sudo netctl start home-wifi"
-alias local-devenv="cd ~/projects/personal/local-devenv/ ; vagrant up ; vagrant ssh"
+alias local-devenv="cd ~/Projects/Personal/local-devenv/ ; vagrant up ; vagrant ssh"
 
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
     ssh-agent > ~/.ssh-agent-thing
