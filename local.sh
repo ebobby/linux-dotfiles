627c6170ceca9bcb3dc387d6a437443ea7baf174
Francisco Soto <ebobby@ebobby.org>
2018-06-28 01:03:03 -0700

    More updates...

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
-alias home-wifi="sudo netctl start home-wifi"
+alias home-wifi="sudo netctl start home-wifi ; sudo netctl wait-online home-wifi"
 alias local-devenv="cd ~/Projects/Personal/local-devenv/ ; vagrant up ; vagrant ssh"
 
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
