557f9842e72ae88da868e66401cdde879912e813
Francisco Soto <ebobby@ebobby.org>
2018-07-01 20:40:48 -0700

    Oryx Pro update.

--- local.sh
+++ local.sh
@@ -1,6 +1,3 @@
-alias home-wifi="sudo netctl start home-wifi ; sudo netctl wait-online home-wifi"
-alias local-devenv="cd ~/Projects/Personal/local-devenv/ ; vagrant up ; vagrant ssh"
-
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
     ssh-agent > ~/.ssh-agent-thing
 fi
