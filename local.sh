4a900e51fd5c60cb16037ae33262eeae9fe71eb9
Francisco Soto <ebobby@ebobby.org>
2018-07-03 15:10:44 -0700

    More Oryx Pro updates.

--- local.sh
+++ local.sh
@@ -1,3 +1,6 @@
+alias local-devenv="cd ~/Projects/local-devenv ; vagrant up ; vagrant ssh"
+alias carezone-devenv="cd ~/Projects/carezone-devenv ; vagrant up ; vagrant ssh"
+
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
     ssh-agent > ~/.ssh-agent-thing
 fi
