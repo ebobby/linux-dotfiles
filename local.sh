330d01393c0dc705c3979b58e3309255ad1eeedd
Francisco Soto <ebobby@ebobby.org>
2018-07-12 00:37:20 -0700

    Gnome keyring and tweaks

--- local.sh
+++ local.sh
@@ -1,14 +1,9 @@
 alias local-devenv="cd ~/Projects/local-devenv ; vagrant up ; vagrant ssh"
 alias carezone-devenv="cd ~/Projects/carezone-devenv ; vagrant up ; vagrant ssh"
 alias nvidia-settings="optirun -b none nvidia-settings -c :8 "
-
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent > ~/.ssh-agent-thing
-fi
-
-if [[ "$SSH_AGENT_PID" == "" ]]; then
-    eval "$(<~/.ssh-agent-thing)"
-fi
+alias profile-performance="sudo system76-power profile performance"
+alias profile-balanced="sudo system76-power profile balanced"
+alias profile-battery="sudo system76-power profile battery"
 
 source /usr/share/nvm/init-nvm.sh
 eval "$(rbenv init -)"
