3f9b09e252adee45b007596a224e57c237d9ba88
Francisco Soto <ebobby@ebobby.org>
2018-07-15 14:01:07 -0700

    Discrete card handling aliases.

--- local.sh
+++ local.sh
@@ -1,9 +1,13 @@
 alias local-devenv="cd ~/Projects/local-devenv ; vagrant up ; vagrant ssh"
 alias carezone-devenv="cd ~/Projects/carezone-devenv ; vagrant up ; vagrant ssh"
 alias nvidia-settings="optirun -b none nvidia-settings -c :8 "
+
 alias profile-performance="sudo system76-power profile performance"
 alias profile-balanced="sudo system76-power profile balanced"
 alias profile-battery="sudo system76-power profile battery"
+
+alias nvidia-on="sudo system76-power graphics power on ; sudo modprobe nvidia-drm nvidia-modeset nvidia"
+alias nvidia-off="sudo rmmod nvidia-drm nvidia-modeset nvidia ; sudo system76-power graphics power off"
 
 source /usr/share/nvm/init-nvm.sh
 eval "$(rbenv init -)"
