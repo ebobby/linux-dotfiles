95e60044e6131d87493f0d4ac1be08ff9b548805
Francisco Soto <ebobby@ebobby.org>
2018-07-28 21:00:50 -0700

    Copy config.

---
+++ copy-from-config.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cp ~/.Xdefaults ./Xdefaults
+cp ~/.asoundrc ./asoundrc
+cp ~/.drirc ./drirc
+cp ~/.gitconfig ./gitconfig
+cp ~/.local.sh ./local.sh
+cp ~/.xinitrc ./xinitrc
+cp ~/.xprofile ./xprofile
+cp ~/.zshrc ./zshrc
+
+cp ~/.config/compton.conf ./config/compton.conf
+cp ~/.config/dunst/dunstrc ./config/dunst/dunstrc
+cp ~/.config/htop/htoprc ./config/htop/htoprc
+cp ~/.config/i3/config ./config/i3/config
+cp ~/.config/nano/nanorc ./config/nano/nanorc
+cp ~/.config/neofetch/config.conf ./config/neofetch/config.conf
+cp -R ~/.config/polybar/* ./config/polybar/
+cp ~/.config/pulse/daemon.conf ./config/pulse/daemon.conf
+cp ~/.config/rofi/config ./config/rofi/config
+cp ~/.config/volumeicon/volumeicon ./config/volumeicon/volumeicon
+cp ~/.config/zoomus.conf ./config/zoomus.conf
