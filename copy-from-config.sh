2e6755f66c47487fc775c9546d5d0568ef6b18df
Francisco Soto <ebobby@ebobby.org>
2019-06-24 22:34:06 -0700

    New files...

--- copy-from-config.sh
+++ copy-from-config.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-cp ~/.Xdefaults ./Xdefaults
+cp ~/.Xresources ./Xresources
 cp ~/.asoundrc ./asoundrc
-cp ~/.gitconfig ./gitconfig
+cp ~/.base-packages.txt ./base-packages.txt
 cp ~/.environment.sh ./environment.sh
+cp ~/.gitconfig ./gitconfig
+cp ~/.tmux.conf ./tmux.conf
 cp ~/.xinitrc ./xinitrc
 cp ~/.xprofile ./xprofile
+cp ~/.xscreensaver ./xcreensaver
 cp ~/.zshrc ./zshrc
 
+cp -R ~/.config/PulseEffects ./config/PulseEffects/
+cp -R ~/.config/i3/* ./config/i3/
+cp -R ~/.config/polybar/* ./config/polybar/
+cp -R ~/.config/tmuxinator/ ./config/tmuxinator/
 cp ~/.config/compton.conf ./config/compton.conf
 cp ~/.config/dunst/dunstrc ./config/dunst/dunstrc
+cp ~/.config/gtk-3.0/gtk.css ./config/gtk-3.0/gtk.css
 cp ~/.config/htop/htoprc ./config/htop/htoprc
-cp -R ~/.config/i3/* ./config/i3/
 cp ~/.config/nano/nanorc ./config/nano/nanorc
 cp ~/.config/neofetch/config.conf ./config/neofetch/config.conf
-cp -R ~/.config/polybar/* ./config/polybar/
+cp ~/.config/pavucontrol.ini ./config/pavucontrol.ini
+cp ~/.config/pulse/daemon.conf ./config/pulse/daemon.conf
 cp ~/.config/pulse/daemon.conf ./config/pulse/daemon.conf
 cp ~/.config/pulse/default.pa ./config/pulse/default.pa
-cp ~/.config/pulse/equalizer-presets.tdb ./config/pulse/equalizer-presets.tdb
-cp ~/.config/pulse/equalizer-state.tdb ./config/pulse/equalizer-state.tdb
-cp ~/.config/pulse/daemon.conf ./config/pulse/daemon.conf
 cp ~/.config/rofi/config ./config/rofi/config
+cp ~/.config/termite/config ./config/termite/config
 cp ~/.config/volumeicon/volumeicon ./config/volumeicon/volumeicon
 cp ~/.config/zoomus.conf ./config/zoomus.conf
-cp ~/.config/pavucontrol.ini ./config/pavucontrol.ini
